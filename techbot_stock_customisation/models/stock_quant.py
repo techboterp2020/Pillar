4d7322b729a955fd40e70573a5c74479543a8102
samrajamal2001 <samrajamal2001@gmail.com>
2025-10-08 16:10:03 +0500

    changes

--- techbot_stock_customisation/models/stock_quant.py
+++ techbot_stock_customisation/models/stock_quant.py
@@ -5,15 +5,41 @@ from odoo.tools import float_compare
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    engine_no = fields.Char(string="Engine No")
-    chassis_no = fields.Char(string="Chassis No")
-    key_no = fields.Char(string="Key No")
-    model_year = fields.Char(string="Model Year")
-    color_internal = fields.Char(string="Internal Color")
-    color_external = fields.Char(string="External Color")
-    make = fields.Char(string='Origin')
-    bill_of_entry = fields.Char(string="BOE")
-    bill_of_lading = fields.Char(string="BOL")
+    engine_no = fields.Char(string="Engine No",compute='_compute_lot_values', store=True)
+    chassis_no = fields.Char(string="Chassis No",compute='_compute_lot_values', store=True)
+    key_no = fields.Char(string="Key No",compute='_compute_lot_values', store=True)
+    model_year = fields.Char(string="Model Year",compute='_compute_lot_values', store=True)
+    color_internal = fields.Char(string="Internal Color",compute='_compute_lot_values', store=True)
+    color_external = fields.Char(string="External Color",compute='_compute_lot_values', store=True)
+    make = fields.Char(string='Origin',compute='_compute_lot_values', store=True)
+    bill_of_entry = fields.Char(string="BOE",compute='_compute_lot_values', store=True)
+    bill_of_lading = fields.Char(string="BOL",compute='_compute_lot_values', store=True)
+
+    @api.depends('lot_id')
+    def _compute_lot_values(self):
+        for rec in self:
+            if rec.lot_id:
+                rec.engine_no = rec.lot_id.engine_no
+                rec.chassis_no = rec.lot_id.chassis_no
+                rec.key_no = rec.lot_id.key_no 
+                rec.model_year = rec.lot_id.model_year
+                rec.color_internal = rec.lot_id.color_internal
+                rec.color_external = rec.lot_id.color_external
+                rec.make = rec.lot_id.make
+                rec.bill_of_entry = rec.lot_id.bill_of_entry 
+                rec.bill_of_lading = rec.lot_id.bill_of_lading
+
+            else:
+
+                rec.engine_no = None
+                rec.chassis_no = None
+                rec.key_no = None
+                rec.model_year = None
+                rec.color_internal = None
+                rec.color_external = None
+                rec.make = None
+                rec.bill_of_entry = None
+                rec.bill_of_lading = None
 
     @api.model
     def _get_inventory_fields_write(self):
